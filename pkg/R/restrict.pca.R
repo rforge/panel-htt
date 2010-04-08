8b3cd8acd53e9e465d11fb84de97f15783e05da1
domr <domr@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-04-08 21:55:30 +0000

    restrict.pca.R als eigene Datei ge-add-ed
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/panel-htt@12 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/restrict.pca.R
@@ -0,0 +1,37 @@
+##########################
+# restrict mode "standard": F'F/T = I and S'S/N = diag
+# restrict mode "inverse" : S'S/N = I and F'F/T = I
+
+restrict.pca <- function(pca.fit.obj, restric.mode= c("standard","inverse.standard"))
+	{
+	if(class(pca.fit.obj)!="pca.fit") stopt(c("The pca.fit.obj is not a 'pca.fit' object"))
+ # pca.fit object  
+	nr    <- nrow(pca.fit.obj$Q.fit)
+	nc    <- ncol(pca.fit.obj$Q.fit)
+	U 	<- pca.fit.obj$U
+	W 	<- pca.fit.obj$W
+	sqr.E <- pca.fit.obj$sqr.E
+	E	<- pca.fit.obj$E
+	Sd2   <- pca.fit.obj$V.d/(nr*nc)
+
+ # restrict eigen values
+	Eval	<- E/(nr*nc)	
+
+ # restric factors and scores
+	re.mo <-match.arg(restric.mode)
+	
+	switch(re.mo,
+			standard=
+			{
+			factors <- U*sqrt(nr)
+			scores  <- W%*%diag(sqr.E)/sqrt(nr)
+			},
+			inverse.standard=
+			{
+			factors <- U%*%diag(sqr.E)/sqrt(nc)
+			scores  <- W*sqrt(nc)
+			}
+		)
+
+	list(factors= factors, scores= scores, Eval= Eval, Sd2 = Sd2)		
+	}
