2eebec02d85bb6f62fe2dea317280096317d7697
domr <domr@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-04-15 16:04:55 +0000

    manual für kss hinzugefühgt
    und in der kss angefangen mich für die weiteren 
    arbeiten vorzubereiten: nur comments für mich selber geschrieben.
    
    
    
    
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/panel-htt@21 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/FUN.kss.R
+++ pkg/R/FUN.kss.R
@@ -4,8 +4,8 @@ FUN.kss <- function(formula, given.d, x.weights=NULL, norder=2, df=norder+2, spa
 	{
 
   # extract data from formula
-	obj.Pformula <- FUN.Pformula(formula= formula, effect = effect)
-        
+	obj.Pformula <- FUN.Pformula(formula = formula, effect = effect)
+       
 	N <- ncol(obj.Pformula[[1]]$ODM)
 	T <- nrow(obj.Pformula[[1]]$ODM)
 	P <- length(obj.Pformula)-1
@@ -27,6 +27,7 @@ FUN.kss <- function(formula, given.d, x.weights=NULL, norder=2, df=norder+2, spa
         TR.X.mat.smth  <- smooth.Pspline(x = seq.int(1,T) 
 						, y = TR.X.mat
                                          , df=10, method=2)$ysmth                       #(T x NP)
+        
 #########################################################################################################
   # calculate beta coefficents
 
@@ -43,16 +44,82 @@ FUN.kss <- function(formula, given.d, x.weights=NULL, norder=2, df=norder+2, spa
         bloc2 <- t.TR.X.TR.Y - t.TR.X.TR.Y.smth     		                       # (Px1)
 
         com.slops.0 <- solve(bloc1)%*%bloc2					       # (Px1)
-        #com.slops.0 <- matrix(com.slops.0, nrow= P,ncol = 1)
+       #com.slops.0 <- matrix(com.slops.0, nrow= P,ncol = 1)
 
   # calculate first step residuals
 
 	Residu.mat    <- matrix((TR.Y - TR.X %*% com.slops.0), T, N)
 
-        obj.gafsa <- gafsa(dat = Residu.mat, dim.crit="KSS.C")
+        obj.gafsa     <- gafsa(dat = Residu.mat, dim.crit="KSS.C")
+        # obj.gafsa: list: dat.fit, x, factors, scores, all.sd2.resi[used.d+1], used.d, d.opt
 
 
 
+## # reestimate beta
+##      factor.stract <- 
+## 	new.y.mat <- TR.Y.mat - factor.stract
+## 	new.y.vec <- as.vector(new.y.mat)
+## 	beta <- qr.solve(X.s, new.y.vec)
+
+## # functional intercept
+##         if(y.in.list$T=="twoway"){
+##           if(is.intercept){
+##             f.intercept.indv      <- y.in.list$OVm + y.mean.twoway.indv.vec - x.all.mean.twoway.indv %*% beta
+##             f.intercept.indv.smth <- smooth.Pspline(x = basis.t , y = f.intercept.indv, df=df.2, method=2)$ysmth
+##             f.intercept.time      <- y.in.list$OVm + y.mean.twoway.time.vec - x.all.mean.twoway.time %*% beta
+##             f.intercept.time.smth <- smooth.Pspline(x = basis.t , y = f.intercept.time, df=df.2, method=2)$ysmth
+##           }else{
+##             f.intercept.indv      <- y.mean.twoway.indv.vec - x.all.mean.twoway.indv %*% beta
+##             f.intercept.indv.smth <- smooth.Pspline(x = basis.t , y = f.intercept.indv, df=df.2, method=2)$ysmth
+##             f.intercept.time      <- y.mean.twoway.time.vec - x.all.mean.twoway.time %*% beta
+##             f.intercept.time.smth <- smooth.Pspline(x = basis.t , y = f.intercept.time, df=df.2, method=2)$ysmth
+##           }          
+##         }else{
+##           if(is.intercept){
+##             f.intercept         <- y.in.list$OVm + y.mean.vec - x.all.mean %*% beta
+##             f.intercept.smth    <- smooth.Pspline(x = basis.t , y = f.intercept, df=df.2, method=2)$ysmth
+##           }else{
+##             f.intercept         <- y.mean.vec - x.all.mean %*% beta
+##             f.intercept.smth    <- smooth.Pspline(x = basis.t , y = f.intercept, df=df.2, method=2)$ysmth
+##           }
+##         }
+        
+## # fitted values
+
+## 	x.beta   <- matrix(x.all.matrix, N*T, P)%*% beta
+
+##         if(y.in.list$T=="twoway"){
+##           if(is.intercept){
+##             y.fitted <- matrix(y.in.list$OVm, T, N) + matrix(f.intercept.time.smth, T, N) +
+##               matrix(f.intercept.indiv.smth, T, N) +  matrix(x.beta, T, N) + factor.stract
+##           }else{
+##             y.fitted <- matrix(f.intercept.time.smth, T, N) + matrix(f.intercept.indiv.smth, T, N) +
+##               matrix(x.beta, T, N) + factor.stract
+##           }
+##         }else{
+##           if(is.intercept){
+##             y.fitted <- matrix(y.in.list$OVm, T, N) + matrix(f.intercept.smth, T, N) +
+##               matrix(x.beta, T, N) + factor.stract
+##           }else{
+##             y.fitted <- matrix(f.intercept.smth, T, N) + matrix(x.beta, T, N) + factor.stract
+##           }
+##         }
+                  
+##         obj <- list(basis.t, y.matrix, x.all.matrix, N,T,P, df.1, df.2, dim.bf, lambda, eig.func, Residu.mat, Resi.smth.mat)
+        
+        
+## par(mfrow= c(2,3))
+## matplot(scores, typ = "l")
+## matplot(eig.func, typ = "l")
+## matplot(factor.stract, typ= "l")
+## plot(f.intercept.smth, typ = "l")
+## matplot(y.fitted, typ = "l")
+## matplot(y.matrix, typ= "l")
+## print(beta)# from simulation: beta = c(-1.5, 5, -1.8)
+##         obj
+
+
+        
   # additive effects      
   
 
