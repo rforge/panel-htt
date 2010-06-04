15fb98eebd0fc00ed8d1a653a7b8cdfbe06e7f9b
domr <domr@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-06-04 13:04:46 +0000

    - smoothing der residuen implementiert:   mit leichtem undersmoothing. (0.8*spar_GCV)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/panel-htt@55 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/fAFactMod.R
+++ pkg/R/fAFactMod.R
@@ -16,9 +16,14 @@ fAFactMod <- function(dat, dim.criterion = c("PC1", "PC2"
 	if(missing(d.max))      d.max 	    <- NULL
 	if(missing(sig2.hat))   sig2.hat    <- NULL
 
+  # smoothing the residuals (small degree of undersmoothing)
+
+        spar.low <- smooth.Pspline(x=seq(0, 1, length.out=nr), y=dat, method=3       )$spar * 0.8
+        dat.smth <- smooth.Pspline(x=seq(0, 1, length.out=nr), y=dat, spar  =spar.low)$ysmth
+
   # pca.fit
 
-	fpca.fit.obj <- fpca.fit(dat,
+	fpca.fit.obj <- fpca.fit(dat           = dat.smth,
                                  given.d       = factor.dim,
                                  restrict.mode = restrict.mode,
                                  allow.dual    = allow.dual)
@@ -57,22 +62,32 @@ fAFactMod <- function(dat, dim.criterion = c("PC1", "PC2"
   # factors and loadings parameters
 
 	if(used.d!=0){
-		factors <- fpca.fit.obj$factors[, 1:used.d, drop= FALSE]
-		loadings  <- fpca.fit.obj$loadings[, 1:used.d, drop= FALSE]
-		dat.fit <- tcrossprod(factors, loadings)
-		sd2 <- fpca.fit.obj$Sd2[used.d+1]
-		R <- list(fitted.values =dat.fit, factors = factors
-			, loadings = loadings, sd2= sd2, given.fdim = factor.dim
-			, optimal.fdim = opt.d, used.fdim = used.d)
+		factors  <- fpca.fit.obj$factors[, 1:used.d, drop= FALSE]
+		loadings <- fpca.fit.obj$loadings[, 1:used.d, drop= FALSE]
+		dat.fit  <- tcrossprod(factors, loadings)
+		sd2      <- fpca.fit.obj$Sd2[used.d+1]
+                
+		R        <- list(fitted.values = dat.fit,
+                                 factors       = factors, 
+                                 loadings      = loadings,
+                                 sd2           = sd2,
+                                 given.fdim    = factor.dim,
+                                 optimal.fdim  = opt.d,
+                                 used.fdim     = used.d)
 		}
 	else{
 		factors <- matrix(0, nr, 1)
 		scores  <- matrix(0, nc, 1)
 		dat.fit <- tcrossprod(factors, scores)
-		sd2 <- fpca.fit.obj$Sd2[used.d+1]
-		R <- list(fitted.values =dat.fit, factors = factors
-			, loadings = loadings, resid.sd2= sd2, given.fdim = factor.dim
-			, optimal.fdim = opt.d, used.fdim = used.d)
+		sd2     <- fpca.fit.obj$Sd2[used.d+1]
+                
+		R       <- list(fitted.values = dat.fit,
+                                factors      = factors,
+                                loadings     = loadings,
+                                resid.sd2    = sd2,
+                                given.fdim   = factor.dim, 
+			        optimal.fdim = opt.d,
+                                used.fdim    = used.d)
 		}
 	R
       }
