6e499e52361d1f6247697a4b661c7e4f35acce9f
domr <domr@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-06-11 08:46:45 +0000

    fAFact.Mod bugg beseitigt "=" auf "=="
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/panel-htt@64 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/fAFactMod.R
+++ pkg/R/fAFactMod.R
@@ -6,8 +6,8 @@ fAFactMod <- function(dat, dim.criterion = c("KSS", "PC1", "PC2", "PC3",  "IC1",
                       factor.dim , d.max,
                       alpha=0.01,
                       sig2.hat,
-                      sig2.hat.mode=c("functional", "classical"),  
-                      restrict.mode= c("restrict.factors","restrict.loadings"), 
+                      sig2.hat.mode = c("functional", "classical"),  
+                      restrict.mode = c("restrict.factors","restrict.loadings"), 
                       allow.dual = TRUE)
 {
   
@@ -34,17 +34,18 @@ fAFactMod <- function(dat, dim.criterion = c("KSS", "PC1", "PC2", "PC3",  "IC1",
         
   # estimation of sig2.hat:      
 
-  # Variance-Estimator traditional
-  if(dim.criterion=="KSS" & sig2.hat.mode="classical"){
-    # Note: no smoothing of the residuals for var-estimation
-    sig2.hat <- svd.pca(dat)$V.d[1]/(nr*nc)
-  }
   # Variance-Estimator Section 3.4 (KSS)
-  if(dim.criterion=="KSS" & sig2.hat.mode="functional"){
+  if(dim.criterion=="KSS" & sig2.hat.mode=="functional"){
     id.smth1  <- smooth.Pspline(x = seq.int(0,1, length.out= nr) , y = diag(1,nr),  spar = spar.low)$ysmth
     id.smth2  <- smooth.Pspline(x = seq.int(0,1, length.out= nr) , y = id.smth1,    spar = spar.low)$ysmth
     tr        <- (nr + sum(diag(id.smth2)) - 2*sum(diag(id.smth1)))
     sig2.hat  <- sum((dat-dat.smth)^2)/((nc-1)*tr)
+  }
+  
+  # Variance-Estimator traditional
+  if(dim.criterion=="KSS" & sig2.hat.mode=="classical"){
+    # Note: no smoothing of the residuals for var-estimation
+    sig2.hat <- svd.pca(dat)$V.d[1]/(nr*nc)
   }
         
   # dimension selection
@@ -76,6 +77,7 @@ fAFactMod <- function(dat, dim.criterion = c("KSS", "PC1", "PC2", "PC3",  "IC1",
                           KSS  = KSS.OptDim(fpca.fit.obj, sig2.hat = sig2.hat,
                             alpha=alpha,  spar.low = spar.low)
                           )
+  print(est.dim)
   opt.d  <- est.dim[1,2]
   used.d <- ifelse(is.null(factor.dim), opt.d, factor.dim)
 
