641d34a4fb8d1d6a2c17a704d7b8f27c76a780a2
domr <domr@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-06-11 09:04:53 +0000

    Kommentiert
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/panel-htt@65 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/FUN.Pformula.R
+++ pkg/R/FUN.Pformula.R
@@ -1,37 +1,36 @@
 FUN.Pformula <- function(formula, effect)
-	{
-        data.fra <- model.frame(formula)
-        dat.term <- attr(data.fra, "terms")	
+  {
+    data.fra <- model.frame(formula)
+    dat.term <- attr(data.fra, "terms")	
 
-# construct data from formula
-	y.matrix <- model.response(data.fra, "numeric")  # dim(response)=TxN and dim(y.matrix)=TxN:
+  # construct data from formula
+    y.matrix <- model.response(data.fra, "numeric")  # dim(response)=TxN and dim(y.matrix)=TxN:
 
   # check the presence of a 'intercept' in the formula
 
-	regressors.mat <- model.matrix(dat.term, data.fra)
-	is.intercept   <- ifelse(colnames(regressors.mat)[1] == "(Intercept)", TRUE, FALSE)
-	if(is.intercept){
-          x.all.matrix <- regressors.mat[,-1]
-        }else{x.all.matrix   <- regressors.mat}
-
+    regressors.mat <- model.matrix(dat.term, data.fra)
+    is.intercept   <- ifelse(colnames(regressors.mat)[1] == "(Intercept)", TRUE, FALSE)
+    if(is.intercept){
+      x.all.matrix       <- regressors.mat[,-1]
+    }else{x.all.matrix   <- regressors.mat}
+    
   # dimention parameters
 
-	N  <- ncol(y.matrix)
-	T  <- nrow(y.matrix)
-        NT <- N*T
-	P  <- as.integer(ncol(x.all.matrix)/N)
+    N  <- ncol(y.matrix)
+    T  <- nrow(y.matrix)
+    NT <- N*T
+    P  <- as.integer(ncol(x.all.matrix)/N)
 
-  # write the 'P' regressors in a list where each component contains a T x N matix
+  # write the intercept and the 'P' regressors in a list, where each component contains one of p+1 T x N matrices
 
-	data.all.mat <- cbind(y.matrix, x.all.matrix)
-	model.in.list <- lapply(1:(P+1), function(z, i) z[,seq((i-1)*N+1,i*N)], z = data.all.mat)
-        data.in.list <- sapply(model.in.list,
-                            function(z) FUN.with.trans(z
-                                                     , N=N
-                                                     , T=T
-                                                     , is.intercept=is.intercept
-                                                     , effect=effect), simplify=FALSE)
-
-
-	data.in.list
-	}
+    data.all.mat  <- cbind(y.matrix, x.all.matrix)
+    model.in.list <- lapply(1:(P+1), function(z, i) z[,seq((i-1)*N+1,i*N)], z = data.all.mat)
+    data.in.list  <- sapply(model.in.list,
+                            function(z) FUN.with.trans(z,
+                                                       N            = N,
+                                                       T            = T,
+                                                       is.intercept = is.intercept,
+                                                       effect       = effect),
+                            simplify=FALSE)
+    data.in.list
+  }
