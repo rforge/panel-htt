50a96881878191c85b255655488676780fab0acf
domr <domr@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-06-11 09:21:21 +0000

    Code strukturiert
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/panel-htt@68 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/FUN.with.trans.R
+++ pkg/R/FUN.with.trans.R
@@ -1,56 +1,57 @@
-FUN.with.trans <- function(z, N, T, is.intercept, effect = c("pooled", "individual", "time", "twoways")) #, "none"
-		{
-                  with.trans <- match.arg(effect)
-                  mean.z <- mean(z);
-                  switch(with.trans	
-                         , pooled = {
-                           if(is.intercept){Z  = z - mean.z}
-                           else{            Z  = z};
-                           liste <- list("Tr"  = "pooled", # Name of *Tr*ansformation
-                                         "I"   = ifelse(is.intercept, TRUE, FALSE), 
-                                         "ODM" = z,        # *O*rig. *D*ata *M*atrix
-                                         "TDM" = Z,        # *T*ransformed *D*ata *M*atrix
-                                         "TDV" = c(Z),     # *T*ransformed *D*ata *V*ector
-                                         "OVm" = mean.z,   # *OV*erall *m*ean
-                                         "TRm" = 0);       # *TR*ansformation *m*ean. 
+FUN.with.trans <- function(z, N, T, is.intercept,
+                           effect = c("pooled", "individual", "time", "twoways")) #, "none"
+  {
+    with.trans <- match.arg(effect)
+    mean.z <- mean(z);
+    switch(with.trans	
+           , pooled = {
+             if(is.intercept){Z  = z - mean.z}
+             else{            Z  = z};
+             liste <- list("Tr"  = "pooled", # Name of *Tr*ansformation
+                           "I"   = ifelse(is.intercept, TRUE, FALSE), 
+                           "ODM" = z,        # *O*rig. *D*ata *M*atrix
+                           "TDM" = Z,        # *T*ransformed *D*ata *M*atrix
+                           "TDV" = c(Z),     # *T*ransformed *D*ata *V*ector
+                           "OVm" = mean.z,   # *OV*erall *m*ean
+                           "TRm" = 0);       # *TR*ansformation *m*ean. 
                            liste
+           }
+           , individual = {
+             if(is.intercept){Z  = z - matrix(colMeans(z), T, N, byrow = TRUE)- mean.z}
+             else{            Z  = z - matrix(colMeans(z), T, N, byrow = TRUE)};
+             liste <- list("Tr"  = "individual", # Name of *Tr*ansformation
+                           "I"   = ifelse(is.intercept, TRUE, FALSE), 
+                           "ODM" = z,            # *O*rig. *D*ata *M*atrix
+                           "TDM" = Z,            # *T*ransformed *D*ata *M*atrix
+                           "TDV" = c(Z),         # *T*ransformed *D*ata *V*ector
+                           "OVm" = mean.z,       # *OV*erall *m*ean
+                           "TRm" = colMeans(z)); # *TR*ansformation *m*ean
+             liste								
                          }
-                         , individual = {
-                           if(is.intercept){Z  = z - matrix(colMeans(z), T, N, byrow = TRUE)- mean.z}
-                           else{            Z  = z - matrix(colMeans(z), T, N, byrow = TRUE)};
-                           liste <- list("Tr"  = "individual", # Name of *Tr*ansformation
-                                         "I"   = ifelse(is.intercept, TRUE, FALSE), 
-                                         "ODM" = z,            # *O*rig. *D*ata *M*atrix
-                                         "TDM" = Z,            # *T*ransformed *D*ata *M*atrix
-                                         "TDV" = c(Z),         # *T*ransformed *D*ata *V*ector
-                                         "OVm" = mean.z,       # *OV*erall *m*ean
-                                         "TRm" = colMeans(z)); # *TR*ansformation *m*ean
-                           liste								
-                         }
-                         , time = {
-                           if(is.intercept){Z  = z - rowMeans(z) - mean.z}
-                           else{            Z  = z - rowMeans(z)};
-                           liste <- list("Tr"  = "time",       # Name of *Tr*ansformation
-                                         "I"   = ifelse(is.intercept, TRUE, FALSE), 
-                                         "ODM" = z,            # *O*rig. *D*ata *M*atrix
-                                         "TDM" = Z,            # *T*ransformed *D*ata *M*atrix
-                                         "TDV" = c(Z),         # *T*ransformed *D*ata *V*ector
-                                         "OVm" = mean.z,       # *OV*erall *m*ean
-                                         "TRm" = rowMeans(z)); # *TR*ansformation *m*ean
-                           liste                                  
-                         }
-                         , twoways = {
-                           if(is.intercept){Z  = z - matrix(colMeans(z), T, N, byrow = TRUE) - rowMeans(z) + mean.z}
-                           else{            Z  = z - matrix(colMeans(z), T, N, byrow = TRUE) - rowMeans(z)};
-                           liste <- list("Tr"  = "twoway",   # Name of *Tr*ansformation
-                                         "I"   = ifelse(is.intercept, TRUE, FALSE), 
-                                         "ODM" = z,          # *O*rig. *D*ata *M*atrix
-                                         "TDM" = Z,          # *T*ransformed *D*ata *M*atrix
-                                         "TDV" = c(Z),       # *T*ransformed *D*ata *V*ector
-                                         "OVm" = mean.z,     # *OV*erall *M*ean
-                                         "TRm" = list("individual" = colMeans(z),
-                                                      "time"       = rowMeans(z)));# *TR*ansformation *m*eans
-                           liste
-                         }
-                         )		 		
-                }
+           , time = {
+             if(is.intercept){Z  = z - rowMeans(z) - mean.z}
+             else{            Z  = z - rowMeans(z)};
+             liste <- list("Tr"  = "time",       # Name of *Tr*ansformation
+                           "I"   = ifelse(is.intercept, TRUE, FALSE), 
+                           "ODM" = z,            # *O*rig. *D*ata *M*atrix
+                           "TDM" = Z,            # *T*ransformed *D*ata *M*atrix
+                           "TDV" = c(Z),         # *T*ransformed *D*ata *V*ector
+                           "OVm" = mean.z,       # *OV*erall *m*ean
+                           "TRm" = rowMeans(z)); # *TR*ansformation *m*ean
+             liste                                  
+           }
+           , twoways = {
+             if(is.intercept){Z  = z - matrix(colMeans(z), T, N, byrow = TRUE) - rowMeans(z) + mean.z}
+             else{            Z  = z - matrix(colMeans(z), T, N, byrow = TRUE) - rowMeans(z)};
+             liste <- list("Tr"  = "twoway",   # Name of *Tr*ansformation
+                           "I"   = ifelse(is.intercept, TRUE, FALSE), 
+                           "ODM" = z,          # *O*rig. *D*ata *M*atrix
+                           "TDM" = Z,          # *T*ransformed *D*ata *M*atrix
+                           "TDV" = c(Z),       # *T*ransformed *D*ata *V*ector
+                           "OVm" = mean.z,     # *OV*erall *M*ean
+                           "TRm" = list("individual" = colMeans(z),
+                          "time" = rowMeans(z)));# *TR*ansformation *m*eans
+             liste
+           }
+           )		 		
+  }
