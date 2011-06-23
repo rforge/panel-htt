14b14d809e80a6d000a44ce06f9303fb75a7d9d3
domr <domr@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-06-23 14:20:34 +0000

    methoden für KSS nun in "KSS.methods.R" außerdem die test-funktionen für KSS nun extern auf meiner festplatte.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/panel-htt@170 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/KSS.methods.R
@@ -0,0 +1,86 @@
+## Methods ========================================================================================
+
+KSS <- function(formula, ...){ UseMethod("KSS")}
+
+print.KSS <- function(x,...){
+  cat("Call:\n")
+  print(x$call)
+  cat("\nAdditive Effects Type:")
+  eff              <- matrix(x$effect)
+  colnames(eff)    <- ""
+  rownames(eff)    <- ""
+  print(eff)
+  cat("\nCoeffs of Regressors :\n")
+  colnames(x$beta) <- "Beta"
+  rownames(x$beta) <- x$names[2:length(x$names)]
+  print(x$beta)
+}
+
+coef.KSS <- function(x,...){
+  coef.list  <- list(beta=x$beta, beta.0=x$beta.0, mu=x$mu, tau=x$tau)
+  names(coef.list$beta) <- x$names[2:length(x$names)]
+  return(coef.list)
+}
+
+summary.KSS <- function(x,...){
+  ## Residuals:
+  Res.outpt <- round((summary(as.vector(x$residuals))), digits=2)[-4]
+  names(Res.outpt) <- c("Min", "1Q", "Median", "3Q", "Max")
+  ## Add-Effect-Type:
+  eff              <- matrix(x$effect)
+  colnames(eff)    <- ""
+  rownames(eff)    <- ""
+  ## Coefficients:
+  se   <- sqrt(diag(x$beta.V))
+  zval <- coef(x)$beta / se
+  TAB  <- cbind("Estimate" = round(as.numeric(coef(x)$beta), digits=2),
+                "StdErr"   = round(se, digits=2),
+                "z.value"  = round(as.numeric(zval), digits=2),
+                "Pr(>z)"   = round(as.numeric(2*pnorm(-abs(zval))), digits=2))  
+  rownames(TAB) <- x$names[2:length(x$names)]
+
+  ## Number of Dimension and Effects-Type:
+  TAB2 <- rbind("Used Factor Dimension: "        = x$fAFactMod$used.fdim,
+                "Estimated Factor Dimension: "   = x$fAFactMod$optimal.fdim,
+                "Additive Effects Type: "        = as.character(x$eff))
+  colnames(TAB2) <- c("")
+  
+  ## Result:
+  result        <- list(call         = x$call,
+                        eff          = eff,
+                        coefficients = TAB,
+                        nDim.effType = TAB2,
+                        Res.outpt    = Res.outpt,
+                        KSS.obj      = x)                
+  class(result) <- "summary.KSS"
+  result
+}
+
+print.summary.KSS <- function(x, ...){
+  ## Call
+  cat("Call:\n")
+  print(x$call)
+  ## Residuals:
+  cat("\nResiduals:\n")
+  print(x$Res.outpt)
+  cat("\n")
+  ## Number of Dimension and Effect-Type
+  print(x$nDim.effType)
+  ## Beta-Coeffs
+  cat("\n Beta-Coefficients\n")
+  printCoefmat(x$coefficients)
+}
+
+plot.summary.KSS <- function(x,...){
+  if(x$KSS.obj$eff=="time"|x$KSS.obj$eff=="twoways"){
+    par(mfrow=c(1,3))
+    plot.ts(x$KSS.obj$beta.0, main="beta.0", ylab="",...)
+  }else{par(mfrow=c(1,2))}
+  matplot(x$KSS.obj$fAFactMod$factors,
+          main=paste("EstimatedFactor-Structure\n(Used Dimension=",x$KSS.obj$fAFactMod$used.fdim,")"),
+          xlab="Time",ylab="", type="l",...)
+  matplot(x$KSS.obj$fAFactMod$fitted.values,
+          main=paste("Fitted individual \nTime-Trends"),
+          xlab="Time",ylab="", type="l",...)
+  par(mfrow=c(1,1))
+}
