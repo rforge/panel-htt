6243fcc724fbab9c5a09dcc7217ef3da22ed86ce
domr <domr@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-04-20 08:08:46 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/panel-htt@257 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/Eup.methods.R
+++ pkg/R/Eup.methods.R
@@ -2,7 +2,21 @@
 
 ## Methods ========================================================================================
 
-Eup <- function(formula, ...){ UseMethod("Eup")}
+Eup <- function(formula,
+		additive.effects = c("none", "individual",
+				     "time", "twoways"),
+		dim.criterion	 = c("PC1", "PC2", "PC3", "IC1","IC2" , "IC3", "IPC1", "IPC2","IPC3" , "ED"),
+		d.max            = NULL
+		sig2.hat         = NULL
+		factor.dim       = NULL
+		double.iteration = TRUE
+		start.beta       = NULL
+		max.iteration    = 500
+		convergence      = 1e-6
+		restrict.mode    = c("restrict.factors","restrict.loadings"),
+                ...){
+  UseMethod("Eup")
+}
 
  print.Eup <- function(x,...){
    cat("Call:\n")
