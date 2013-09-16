fdfe31a6c8a5b8f6b72f6b346bd6f0590958f423
bada <bada@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-09-16 10:30:16 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/panel-htt@330 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/Eup.methods.R
+++ pkg/R/Eup.methods.R
@@ -4,8 +4,7 @@
 
 Eup <- function(formula,
     additive.effects = c("none", "individual", "time", "twoways"),
-    dim.criterion    = c("PC1", "PC2", "PC3", "IC1", "IC2" , "IC3",
-"IPC1", "IPC2", "IPC3" , "ED"),
+    dim.criterion    = c("PC1", "PC2", "PC3", "BIC3", "IC1", "IC2" , "IC3", "IPC1", "IPC2", "IPC3"),
     d.max            = NULL,
     sig2.hat         = NULL,
     factor.dim       = NULL,
